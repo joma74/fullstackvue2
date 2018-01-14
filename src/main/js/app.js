9dd87e406a1f1f08fd6bb5502349789485ab7f7a
joma74 <dev-mails@gmx.net>
2018-01-15 00:28:24 +0100

    feat(content): modal window implemented
    - overlays body by block being fixed at 0,0,0,0
    - body is fixed against scrolling meanwhile by
      adding an overflow of hidden via document.body.classList

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -5,10 +5,21 @@ var app = new Vue({
       "background-image": "url('../../test/images/header.jpg')"
     },
     contracted: true,
+    modalOpen: false,
     title: sample.title,
     address: sample.address,
     about: sample.about,
     amenities: sample.amenities,
     prices: sample.prices
+  },
+  watch: {
+    modalOpen: function() {
+      var className = "modal-open";
+      if (this.modalOpen) {
+        document.body.classList.add(className);
+      } else {
+        document.body.classList.remove(className);
+      }
+    }
   }
 });
